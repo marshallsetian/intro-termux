19cfa3f8d0dfa56eeda62b7cdebe377e4b6b67b2
marshall setian <marshallsetian@gmail.com>
2022-10-27 12:28:19 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -36,8 +36,7 @@ os.system("xdg-open https://www.facebook.com/yoelmars1509")
 autoketik(f"{biru}[{kuning}Warning{biru}] {W}Thx yang udah FOLLOW YAH.., semoga work")
 time.sleep(3)
 os.system("clear")
-autoketik(f"""
------------------------------------------
+
 {putih}[{biru}•{putih}] {biru}Author {putih}   : MarshallSetian
 {putih}[{biru}•{putih}] {abu}GitHub {putih}   : papaENG
 {putih}[{biru}•{putih}] {merah}You{putih}Tube {putih}  : marshall setian
